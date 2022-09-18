e5a6591d657921e40ac567d2a09d19a861ae0d46
Prayag Bhatia <prayag.bhatia2015@vit.ac.in>
2022-09-18 13:36:05 +0530

    ADD : changes for uppercase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 //Navbar component is being imported from Navbar.js
 //props are being passed to components to render dynamic flow
+// container my-3 -- boostrap class for margins and containers
 
 function App() {
   return (
     <>
     <Navbar title="TextUtils" aboutText="About Us" /> 
+    <div className="container my-5">
+    <TextForm heading="Enter the text to analyze"/>
+    </div>
     </>
   );
 }
