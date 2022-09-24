0b31f64da7d5bdb4196b321d348cd85f29f6eacb
Prayag Bhatia <prayag.bhatia2015@vit.ac.in>
2022-09-24 11:35:26 +0530

    FIX : optimizng dark mode && ADD : alerts on action

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import Alert from './components/Alert';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
@@ -11,21 +12,38 @@ import TextForm from './components/TextForm';
 function App() {
 
   const [mode, setMode] = useState('light') ; //initially setting dark mode as false
+  const [alert, setAlert] = useState(null) ;
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500) ; //alert remains only for 1.5 secs
+
+  }
 
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark')
+      document.body.style.backgroundColor = 'grey';
+      showAlert("Dark mode has been enabled", "success")
     }
     else {
       setMode('light')
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success")
     }
   }
 
   return (
     <>
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} /> 
+    <Alert alert={alert} />
     <div className="container my-5">
-    <TextForm heading="Enter the text to analyze"/>
+    <TextForm heading="Enter the text to analyze" mode={mode} alert={showAlert}/>
     {/* <About/> */}
     </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,18 +5,21 @@ export default function TextForm(props) {
   const handleUpClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
+    props.alert("converted to uppercase !", "success")
 
   }
 
   const handleLowerClick = () => {
     let newText = text.toLowerCase();
     setText(newText);
+    props.alert("converted to lowercase !", "success")
 
   }
 
   const handleRemoveSpecialCharcters = () => {
     let newText = text.replace(/[^a-zA-Z ]/g, "");
     setText(newText);
+    props.alert("all special characters removed !", "success")
   }
 
   const handleOnChange = (event) => {
@@ -27,21 +30,25 @@ export default function TextForm(props) {
   //text is the current value being used ; curr it is Enter Text here
   //setText is a fucntion used to update text value
 
+  //2 curly braces in return func 
+  //1st -- for referring javascript and 2nd is for object
+
   return (
     <>
-    <div className="container">
+    <div className="container" style={{color: props.mode === 'dark' ? 'white' : 'black'}}>
         <h2>{props.heading}</h2>
         <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" ></textarea>
+    <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark' ? 'grey' : 'white',
+        color: props.mode === 'dark' ? 'white' : 'black'}} id="myBox" rows="8" ></textarea>
         </div>
         <button className='btn btn-primary mx-2' onClick={handleUpClick}> Convert to Upper case</button>
         <button className='btn btn-primary mx-2' onClick={handleLowerClick}> Convert to Lower case</button>
         <button className='btn btn-primary mx-2' onClick={handleRemoveSpecialCharcters}> Remove Special Characters</button>
        
     </div>
-    <div className='container my-5'>
+    <div className='container my-5' style={{color: props.mode === 'dark' ? 'white' : 'black'}}>
       <h3>Preview</h3>
-      <p>{text}</p>
+      <p>{text.length > 0 ? text : "Enter something to preview"}</p>
       <h3> Your Text Summary</h3>
       <p>{text.split(" ").length} words and {text.length} characters</p>
       <p>{0.008 * text.split(" ").length} Minutes read</p>
